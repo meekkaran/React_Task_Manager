b9cf82a473dd53a77f657838b255d1569f8e8679
Hope Kenga <kengahope7@gmail.com>
2023-06-29 21:22:34 +0300

    chore: fix Postgres error and add error handling

--- backend/app.py
+++ backend/app.py
@@ -1,58 +1,72 @@
-from flask import Flask,jsonify,request
+from flask import Flask,jsonify, request
 from prisma import PrismaClient
 
 app = Flask(__name__)
 prisma = PrismaClient()
 
 #Create a task
-@app.route('/tasks', methods=['GET'])
+@app.route('/tasks', methods=['POST'])
 def create_task():
     data = request.get_json()
     title = data.get('title')
-    status=data.get('status')
-    task = prisma.task.create(
-        {
-        "data": {
-            "title": title,
-            "status": status
-        }
-        }
-    )
-    return jsonify(task)
+    status = data.get('status')
+
+    if not title or not status:
+        return jsonify({'error': 'Title and status are required'}), 400
+    
+    try:
+        task  = prisma.task.create(
+            {
+                'data': {
+                    "title": title,
+                    "status": status
+                }
+            }
+        )
+        return jsonify(task)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
 
 #Update Task
-
-@app.route('/tasks/<int:task_id>', methods=['PUT'])
+@app.route('/tasks/<int:task_id>', methods=['PUT'])    
 def update_task(task_id):
     data = request.get_json()
     title = data.get('title')
-    status=data.get('status')
-    task = prisma.task.update(
-        {
-            "where": {
-                "id": task_id
-            },
-            "data": {
-                "title" : title,
-                "status" : status
-            }
-        }
-    )
-    return jsonify(task)
+    status = data.get('status')
 
+    if not title or not status:
+        return jsonify({'error': 'Title and status are required'}),400
+    try:
+        task = prisma.task.update(
+            {
+                "where": {
+                    "id": task_id,
+                },
+                "data": {
+                    "title": title,
+                    "status": status
+                }
+            }
+        )
+        return jsonify(task)
+    except Exception as e:
+        return jsonify({'error':str(e)}), 500
 
 #Delete Task
 @app.route('/tasks/<int:task_id>', methods=['DELETE'])
-def delete_taks(task_id):
-    task = prisma.task.delete(
-        {
-            "where": {
-                "id": task_id
+def delete_task(task_id):
+    try:
+        task = prisma.task.delete(
+            {
+                "where":{
+                    "id":task_id
+                }
             }
-        }
-    )
-    return jsonify(task)
-
+        )
+        return jsonify(task)
+    except Exception as e:
+        return jsonify({'error', str(e)}), 500
+    
 if __name__ == '__main__':
     app.run()
-
